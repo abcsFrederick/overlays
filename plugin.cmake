bfa8b31e027b89b0bfebf3583d0cae9449164a76
Miao <tymiao@gwmail.gwu.edu>
2019-08-29 14:22:03 -0400

    test

--- plugin.cmake
+++ plugin.cmake
@@ -1,20 +1,20 @@
 # add_standard_plugin_tests()
 
-add_python_style_test(
-  python_static_analysis_overlays_tests
-  "${PROJECT_SOURCE_DIR}/plugins/overlays/plugin_tests"
-)
-add_python_test(overlays
-  PLUGIN overlays
-  BIND_SERVER EXTERNAL_DATA
-  "plugins/overlays/image.tiff"
-  "plugins/overlays/overlay.tiff"
-)
-add_web_client_test(
-  panel_test
-  "${CMAKE_CURRENT_LIST_DIR}/plugin_tests/panelSpec.js"
-  PLUGIN overlays
-)
+# add_python_style_test(
+#   python_static_analysis_overlays_tests
+#   "${PROJECT_SOURCE_DIR}/plugins/overlays/plugin_tests"
+# )
+# add_python_test(overlays
+#   PLUGIN overlays
+#   BIND_SERVER EXTERNAL_DATA
+#   "plugins/overlays/image.tiff"
+#   "plugins/overlays/overlay.tiff"
+# )
+# add_web_client_test(
+#   panel_test
+#   "${CMAKE_CURRENT_LIST_DIR}/plugin_tests/panelSpec.js"
+#   PLUGIN overlays
+# )
 
 add_web_client_test(
   save_overlay_test
@@ -22,14 +22,14 @@ add_web_client_test(
   PLUGIN overlays
 )
 
-add_web_client_test(
-  imageView_test
-  "${CMAKE_CURRENT_LIST_DIR}/plugin_tests/imageViewSpec.js"
-  PLUGIN overlays
-  TEST_MODULE "plugin_tests.web_client_test"
-  TEST_PYTHONPATH "${CMAKE_CURRENT_LIST_DIR}"
-)
+# add_web_client_test(
+#   imageView_test
+#   "${CMAKE_CURRENT_LIST_DIR}/plugin_tests/imageViewSpec.js"
+#   PLUGIN overlays
+#   TEST_MODULE "plugin_tests.web_client_test"
+#   TEST_PYTHONPATH "${CMAKE_CURRENT_LIST_DIR}"
+# )
 
-add_eslint_test(
-  js_static_analysis_overlays_tests "${PROJECT_SOURCE_DIR}/plugins/overlays"
-)
+# add_eslint_test(
+#   js_static_analysis_overlays_tests "${PROJECT_SOURCE_DIR}/plugins/overlays"
+# )
